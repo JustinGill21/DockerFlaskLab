59e667a197912b6cd525e58b36431ce0c4639c0c
JustinGill21 <jugi0273@colorado.edu>
2022-04-04 16:27:53 -0600

    Second commit

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 from flask import Flask, jsonify, request
+# As specified in the docker-compose.yml file, FLASK_ENV was set to development
+# So, we need to disable the app's Debug environment variable
+# Reference: https://flask.palletsprojects.com/en/2.1.x/config/
+
+DEBUG = False
+
 
 app = Flask(__name__)
+'''
+App Requirements:
+1.  All routes must return JSON
+2.  All routes must have a docstring
+
+'''
+
+
+
 
 @app.route('/')
 def hello_world():
-    return 'Flask Docker Container says: Hello World!'
+    return jsonify('Flask Docker Container says: Hello World!')
+
+@app.route('/ping', methods=['GET'])
+def ping_pong_route():
+    return jsonify('pong!')
+    
+if __name__ == '__main__':
+    app.run()
